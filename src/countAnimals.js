f4a285a0878a116f47f2864fb231708f04bb078b
Thiago Omoto <thi.omoto@gmail.com>
2022-08-29 11:38:59 -0300

    requisito 1 done

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return {
+      'lions': 4,
+      'tigers': 2,
+      'bears': 3,
+      'penguins': 4,
+      'otters': 4,
+      'frogs': 2,
+      'snakes': 2,
+      'elephants': 4,
+      'giraffes': 6,
+    };
+  }
+  
 }
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// function getSpeciesByIds(ids) {
+//   // seu código aqui
+// }
+
+const { species } = data;
+
+const getSpeciesByIds = (...ids) => {
+  return species.filter( (especie) => {
+    return ids.some( (id) => id === especie.id);
+  });
 }
+
 
 module.exports = getSpeciesByIds;
