524fab31e4c105a6d360a831e1d5ee88e7a70f09
Thiago Omoto <thi.omoto@gmail.com>
2022-08-29 13:57:45 -0300

    requisito 12 done

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,33 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  test('Teste não passando argumentos', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
+  test('Verifica se o zoo está fechado na segunda', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+  test('Verifica se o zoo está aberto na terça', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+  });
+  test('Verifica se o zoo está fechado na quarta a noite', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+//   test ('', () => {
+//     expect().toBe();
+//   });
+//   test ('', () => {
+//     expect().toBe();
+//   });
+//   test ('', () => {
+//     expect().toBe();
+//   });
+});
