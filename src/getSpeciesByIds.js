5f19e5d619acd99ec040df19cc1eb5c22e4daa94
Thiago Omoto <thi.omoto@gmail.com>
2022-08-29 11:45:31 -0300

    arrumando o lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,11 +6,7 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-const getSpeciesByIds = (...ids) => {
-  return species.filter( (especie) => {
-    return ids.some( (id) => id === especie.id);
-  });
-}
-
+const getSpeciesByIds = (...ids) => species
+  .filter((especie) => ids.some((id) => id === especie.id));
 
 module.exports = getSpeciesByIds;
