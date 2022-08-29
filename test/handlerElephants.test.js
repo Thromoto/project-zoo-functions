5c3158d236116a4f415b37f7130294e9caeaf5b8
Thiago Omoto <thi.omoto@gmail.com>
2022-08-29 13:11:51 -0300

    requisito 7 done

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,4 +1,5 @@
 // const { TestScheduler } = require('jest');
+// const { species } = require('../data/zoo_data');
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
@@ -10,5 +11,11 @@ describe('Testes da função HandlerElephants', () => {
   });
   test('retorna a média de idade dos elefantes', () => {
     expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+  test('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  test('Para o argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
   });
 });
