92f6d5d48af445c2a2120e87c717d9e8e013ecf8
Thiago Omoto <thi.omoto@gmail.com>
2022-08-29 12:54:13 -0300

    requisito 6 done

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,14 @@
+// const { TestScheduler } = require('jest');
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  test('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  test('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+});
