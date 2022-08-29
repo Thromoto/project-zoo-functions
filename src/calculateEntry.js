29577c2d0ba5e1b397117ee348ed549da8ca0f88
Thiago Omoto <thi.omoto@gmail.com>
2022-08-29 19:13:51 -0300

    requisito 4 done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,4 +1,14 @@
+// const en = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
+
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
 function countEntrants(entrants) {
   // seu código aqui
