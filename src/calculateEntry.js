3df001c961ca4d3a5f6102329fefdf271169a56d
Thiago Omoto <thi.omoto@gmail.com>
2022-08-31 11:30:20 -0300

    requisito 8 done (finally)

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -10,23 +10,55 @@ const data = require('../data/zoo_data');
 //   { name: 'Carlos Nogueira', age: 50 },
 // ];
 
-const child = (crianca) => crianca.filter((contagem) => contagem.age < 18).length;
-const adult = (adulto) => adulto.filter((contagem) => contagem.age >= 18 && contagem.age < 50)
+const c = (crianca) => crianca.filter((contagem) => contagem.age < 18).length;
+const a = (adulto) => adulto.filter((contagem) => contagem.age >= 18 && contagem.age < 50)
   .length;
-const senior = (velho) => velho.filter((contagem) => contagem.age >= 50).length;
+const s = (velho) => velho.filter((contagem) => contagem.age >= 50).length;
 
 function countEntrants(entrants) {
   // seu código aqui
   const entradaPessoas = {
-    child: child(entrants),
-    adult: adult(entrants),
-    senior: senior(entrants),
+    child: c(entrants),
+    adult: a(entrants),
+    senior: s(entrants),
   };
   return entradaPessoas;
 }
 
+const { prices } = data;
+
+// prices: {
+//   adult: 49.99,
+//   senior: 24.99,
+//   child: 20.99,
+// },
+
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  // if (Object.keys(entrants).length === 0) {
+  //   return 0;
+  // }
+  // if (entrants) {
+  return a(entrants) * prices.adult + c(entrants) * prices.child + s(entrants) * prices.senior;
+  // }
+  // if (entrants) {
+  // return adult(entrants) * prices.adult || child(entrants) * prices.child || senior(entrants) * prices.senior;
+  // }
 }
+
+// const cashAdult = (entrants) => adult * prices.adult;
+// const cashChild = child * prices.child;
+// const cashSenior = senior * prices.senior;
+
+// const valores = {
+//   child: prices.child,
+//   adult: prices.adult,
+//   senior: prices.senior,
+// }
+// return valores;
+// console.log(valores);
 
 module.exports = { calculateEntry, countEntrants };
